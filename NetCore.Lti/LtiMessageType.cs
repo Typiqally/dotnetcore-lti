22724635f0b867e50628a3e14989b0a94ad09b49
Jelle Maas <typiqally@gmail.com>
2023-04-11 22:47:16 +0200

    Add submission review message type

--- NetCore.Lti/LtiMessageType.cs
+++ NetCore.Lti/LtiMessageType.cs
@@ -4,4 +4,5 @@ public static class LtiMessageType
 {
     public const string ResourceLink = "LtiResourceLinkRequest";
     public const string DeepLinking = "LtiDeepLinkingRequest";
+    public const string SubmissionReview = "LtiSubmissionReviewRequest";
 }
