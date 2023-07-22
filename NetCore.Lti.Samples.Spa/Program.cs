189492a9f0849e5f8d03df6d4c0d95c6730e9b81
Jelle Maas <typiqally@gmail.com>
2023-07-22 22:28:52 +0200

    Add auth and token endpoint URI's to configuration

--- NetCore.Lti.Samples.Spa/Program.cs
+++ NetCore.Lti.Samples.Spa/Program.cs
@@ -63,8 +63,8 @@ builder.Services.AddAuthentication(static options => { options.DefaultScheme = C
         options.ClientId = config["Canvas:OAuth2:ClientId"];
         options.ClientSecret = config["Canvas:OAuth2:ClientSecret"];
         options.CallbackPath = "/login/canvas";
-        options.AuthorizationEndpoint = "http://canvas.docker/login/oauth2/auth";
-        options.TokenEndpoint = "http://canvas.docker/login/oauth2/token";
+        options.AuthorizationEndpoint = builder.Configuration["Canvas:OAuth2:AuthorizationEndpoint"];
+        options.TokenEndpoint = builder.Configuration["Canvas:OAuth2:TokenEndpoint"];
         options.SaveTokens = true;
     });
 
