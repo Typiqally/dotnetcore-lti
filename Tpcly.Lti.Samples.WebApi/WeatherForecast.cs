9a0abd7bdf49f27780fbaab4805cc0ef6f849f9a
Jelle Maas <typiqally@gmail.com>
2023-10-17 14:55:26 +0200

    Add initial Web API project from template

---
+++ Tpcly.Lti.Samples.WebApi/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Tpcly.Lti.Samples.WebApi;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
